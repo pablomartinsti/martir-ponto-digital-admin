1a974a0f0fef92b0abbfa57e86c3dfd9ec72efe9
PABLO FERREIRA MARTINS <160162036+pablomartinsti@users.noreply.github.com>
2025-03-27 15:12:01 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import GlobalStyle from "./styles/globalStyles";
-import App from "./app";
+import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
 
 createRoot(document.getElementById("root")!).render(
